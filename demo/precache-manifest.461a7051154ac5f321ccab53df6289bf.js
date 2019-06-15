55e73a186463e633f88dd58b886b653bb7537018
Prithvi Anand <prithvi.anand@proptiger.com>
2019-06-15 21:19:08 +0530

    added demo

---
+++ demo/precache-manifest.461a7051154ac5f321ccab53df6289bf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01413489a0e8c24c5432e78474040e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979bb75b1631f675668d",
+    "url": "/static/css/main.78eb9f1a.chunk.css"
+  },
+  {
+    "revision": "e8a540332de1e3c70ad3",
+    "url": "/static/js/2.64139b0a.chunk.js"
+  },
+  {
+    "revision": "979bb75b1631f675668d",
+    "url": "/static/js/main.57ab1fc4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "557d7cae2e020e9d6e91ac650809c6fe",
+    "url": "/static/media/1.557d7cae.gif"
+  },
+  {
+    "revision": "b7ec0111928238ae61af8790c044f7b3",
+    "url": "/static/media/1.b7ec0111.png"
+  },
+  {
+    "revision": "0d19b438cf0318f71e2ad35ef5f892b4",
+    "url": "/static/media/2.0d19b438.png"
+  },
+  {
+    "revision": "975f1d2cc6b3e5dc0b395b9105e31f82",
+    "url": "/static/media/3.975f1d2c.png"
+  },
+  {
+    "revision": "a0693ea02a86a751bbc5ec3114ad276c",
+    "url": "/static/media/3.a0693ea0.gif"
+  },
+  {
+    "revision": "3e0fc1e601a410eac658b3a2f1c73309",
+    "url": "/static/media/4.3e0fc1e6.png"
+  },
+  {
+    "revision": "eff778681d8e357b9c79a0b330498212",
+    "url": "/static/media/4.eff77868.jpeg"
+  },
+  {
+    "revision": "47051482231e1fc5c5551b637b0a9431",
+    "url": "/static/media/5.47051482.png"
+  },
+  {
+    "revision": "c26edff629cb4afe7d40eaaecd8bc627",
+    "url": "/static/media/5.c26edff6.gif"
+  },
+  {
+    "revision": "4bb8fc9c1a9be034081e57ce2c5d25ec",
+    "url": "/static/media/6.4bb8fc9c.png"
+  },
+  {
+    "revision": "ad716f25b9480f6ea03872bef9857741",
+    "url": "/static/media/6.ad716f25.jpg"
+  },
+  {
+    "revision": "34d1828151225f7ce558bbdd0f8d6c89",
+    "url": "/static/media/7.34d18281.png"
+  },
+  {
+    "revision": "dfba34e81a32acc8c7afc3918b02871a",
+    "url": "/static/media/7.dfba34e8.gif"
+  },
+  {
+    "revision": "58c3ff05f9ec7a97109c40868514f7b4",
+    "url": "/static/media/8.58c3ff05.png"
+  },
+  {
+    "revision": "96f26891226061c551f46b12de120d1c",
+    "url": "/static/media/8.96f26891.gif"
+  },
+  {
+    "revision": "13ce4fde6442ce74178c56f146c24c52",
+    "url": "/static/media/9.13ce4fde.gif"
+  },
+  {
+    "revision": "54a4c0303af707c8f03af9e0d24b6bfa",
+    "url": "/static/media/bck.54a4c030.jpg"
+  },
+  {
+    "revision": "6977cbfe125d2fe2a63abaf92c69645d",
+    "url": "/static/media/bck.6977cbfe.jpg"
+  }
+]);
